714583d638f15e696721d5c64d957f08dd9454f2
DESKTOP-57BD801\user <cecilfayedclemente@gmail.com>
2021-10-25 18:54:20 +0800

    css changes

--- src/components/widget/Logs.vue
+++ src/components/widget/Logs.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="logs-container">
+	<div class="logs-container" v-show="battleStart">
       <p v-for="(log, index) in logs.slice().reverse()" class='log' :key="`log-${index}`">{{log}}</p>
     </div>
 </template>
@@ -15,19 +15,26 @@
 		setup() {
             const service = useMonsterSlayerService();
 			const store = useStore();
+			const battleStart = computed(() => {
+				return store.state.game.battleStart;
+			});
 			const logs = computed(() =>store.state.game.fightLogs);
             return {
-                logs
+                logs,battleStart
 			}
+		
         }
 	})
 	export default LogsWidget;
 </script>
 <style scoped>
  .logs-container {
-        width: 50%;
 		margin: 1rem;
 		background: white;
 		white-space: normal;
+		overflow-x: hidden ;
+		overflow-y: auto;
+		max-height: 200px;
+		width: 50%;
 	}
 </style>
